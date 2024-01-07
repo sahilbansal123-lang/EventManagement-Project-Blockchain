d462e20553c520e74f4827211d752bfb3325b790
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-01-07 10:21:54 +0530

    update

--- src/Component/FilterBox/FilterBox.jsx
+++ src/Component/FilterBox/FilterBox.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import "./FilterBox.css";
 import { dataRender, months, years } from "../../utils/dataRender";
 
-const FilterBox = () => {
+const FilterBox = ({ getMonthYear }) => {
+  const [selectedMonth, setSelectedMonth] = useState("January");
+  const [selectedYear, setSelectedYear] = useState(2024);
+
   const monthToDisplay = () => {
     return dataRender(months);
   };
@@ -11,17 +15,39 @@ const FilterBox = () => {
     return dataRender(years);
   };
 
+  const handleMonthChange = (e) => {
+    console.log(e.target.value);
+    setSelectedMonth(e.target.value);
+  };
+
+  const handleYearChange = (e) => {
+    console.log(e.target.value);
+    setSelectedYear(e.target.value);
+  };
+
+  const updateParent = (e) => {
+    e.preventDefault();
+    getMonthYear(selectedMonth, selectedYear);
+  };
+
   return (
     <div>
-      <form>
-        <label htmlFor="month">Month : </label>
-        <select name="" id="">
-          {monthToDisplay()}
-        </select>
-        <label htmlFor="year">Year : </label>
-        <select name="" id="">
-          {YearsToDisplay()}
-        </select>
+      <form className="filter-card" onSubmit={updateParent}>
+        <div className="wrapper">
+          <div className="date">
+            <label htmlFor="month">Month : </label>
+            <select value={selectedMonth} onChange={handleMonthChange}>
+              {monthToDisplay()}
+            </select>
+          </div>
+          <div className="date">
+            <label htmlFor="year">Year : </label>
+            <select value={selectedYear} onChange={handleYearChange}>
+              {YearsToDisplay()}
+            </select>
+          </div>
+        </div>
+        <button type="submit">Find Events</button>
       </form>
     </div>
   );
--- src/pages/FilterEvents/FilterEvents.jsx
+++ src/pages/FilterEvents/FilterEvents.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import FilterBox from "../../Component/FilterBox/FilterBox";
 import SearchEventList from "../../Component/SearchEventList/SearchEventList";
 import "./FilterEvents.css";
+import { useState } from "react";
 
 const FilterEvents = () => {
+  const [monthYear, setMonthYear] = useState({
+    selectedMonth: null,
+    seletedYear: null,
+  });
+
+  const getMonthYear = (selectedMonth, selectedYear) => {
+    console.log(selectedYear, selectedMonth);
+    setMonthYear({ selectedYear, selectedMonth });
+  };
+
   return (
     <div>
-      <h1>Filter Events</h1>
-      <FilterBox />
-      <SearchEventList />
+      <div className="find-events-wrapper">
+        <FilterBox getMonthYear={getMonthYear} />
+        <SearchEventList monthYear={monthYear} />
+      </div>
     </div>
   );
 };
